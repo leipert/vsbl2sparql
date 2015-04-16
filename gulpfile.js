91d684255d72b4bb5746fc0b102311f723521817
Lukas Eipert <git@leipert.io>
2015-04-16 12:58:35 +0200

    Release 0.2.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 
-var include = require('gulp-file-include');
-var rename = require('gulp-rename');
 
 gulp.task('default', function () {
+    var include = require('gulp-file-include');
+    var rename = require('gulp-rename');
+
     gulp.src(['tpl/**.tpl'])
         .pipe(include({
             prefix: '@@',
@@ -13,4 +14,14 @@ gulp.task('default', function () {
             extname: ".js"
         }))
         .pipe(gulp.dest('./'));
+});
+
+gulp.task('precommit',['setBinaryVersion']);
+
+gulp.task('setBinaryVersion',function(){
+    var replace = require('gulp-replace');
+    var version = require('./package.json').version;
+    return gulp.src(['bin/*'])
+        .pipe(replace(/version\s*:\s*'?[\w\.]+'?/,'version: \''+ version + '\''))
+        .pipe(gulp.dest('bin'))
 });
